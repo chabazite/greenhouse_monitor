5672647d3e61d49636f9f2ee178331d61d98f49b
chabazite <ingallsandrew0@gmail.com>
2023-11-28 11:40:18 -0600

    adding new element for prediction

--- src/webapp/app.js
+++ src/webapp/app.js
@@ -14,17 +14,20 @@ ws.onerror = (error) => {
 ws.onmessage = (event) => {
     try {
         const data = JSON.parse(event.data);
-        // Update the web page with new data
+        // Existing data updates
         document.getElementById('heat-index').textContent = data.heat_index.toFixed(2);
         document.getElementById('humidity').textContent = data.humidity;
         document.getElementById('temperature').textContent = data.temperature.toFixed(2);
         document.getElementById('datetime').textContent = data.datetime;
-        document.getElementById('prediction').textContent = prediction.max_mean.toFixed(2);
+        // New prediction data updates
+        if(data.mean_max) {
+            document.getElementById('prediction').textContent = data.mean_max.toFixed(2);
+        }
     } catch (error) {
         console.error('Error parsing WebSocket data:', error);
     }
 };
-
+   
 ws.onclose = () => {
     console.log('WebSocket connection closed');
 };
